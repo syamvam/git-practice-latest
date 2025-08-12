771b4c10d089cb9fea7f42073cd6397b6adbeae9
syamkumary <syamyala7@gmail.com>
2025-08-12 11:05:16 +0530

    code modified

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 PERSON1=Ramesh
 PERSON2=Suresh
-
 echo "$PERSON1 hi $PERSON2 how are you"
 echo "Hello $PERSON1 i am fine how are you doing"
 echo "I AM GOOD $PERSON2 nice to meet you i am doing good"
