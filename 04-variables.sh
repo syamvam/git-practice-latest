6429208aa3bba32b4db83797e394f06342a9ed20
syamkumary <syamyala7@gmail.com>
2025-08-12 11:19:03 +0530

    arguements code

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 hi $PERSON2 how are you"
+echo "Hello $PERSON1 i am fine how are you doing"
+echo "I AM GOOD $PERSON2 nice to meet you i am doing good"
