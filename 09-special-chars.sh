6136e5829d73594b40cab7ef1fb82beae1f5b0c6
syamkumary <syamyala7@gmail.com>
2025-08-13 11:41:04 +0530

    modified

--- 09-special-chars.sh
+++ 09-special-chars.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "ALL VARIABLES PASSED TO THE SCRIPT $*"
 echo "Number of variables passed $#"
+
+echo "Script Name $0"
+
+echo "directory path $PWD"
+
+echo "home directory of current user $HOME"
+
+echo "PID of script executing $$"
