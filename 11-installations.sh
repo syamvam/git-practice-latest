9240e0a85abae02c2d05465dbf86f84f099eec29
syamkumary <syamyala7@gmail.com>
2025-08-15 12:47:22 +0530

    mysql install script

--- 11-installations.sh
+++ 11-installations.sh
@@ -8,5 +8,12 @@ then
    exit 1
 fi
 
-dnf install git -y
+dnf list installed git
 
+if[ $? -ne 0 ]
+then
+   echo "git is not installed going to install"
+   dnf install git -y
+else
+   echo "git is already installed"
+fi   
