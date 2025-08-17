382dc276802d1861a755d9accd4567f04042c2f9
syamkumary <syamyala7@gmail.com>
2025-08-17 17:45:41 +0530

    shell script changes

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e # setting the automatic exit, if we get error,set -ex for deug
 
+failure(){
+      echo "Failed at $1:$2"
+}
+trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR #ERR IS THE ERROR SIGNAL
+
 echo "hellow world succces"
 echoooo "Hello world failure"
 echo "hello world success"
