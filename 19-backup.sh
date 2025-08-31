3d346186c656654309e51844d3c45b1ed7930a68
syamkumary <syamyala7@gmail.com>
2025-08-31 19:32:19 +0530

    backup

--- 19-backup.sh
+++ 19-backup.sh
@@ -1,32 +1,39 @@
-#!bin/bash
+#!/bin/bash
 
 SOURCE_DIR=$1
-DESTINATION_DIR=$2
-DAYS=${3:-14}
+DEST_DIR=${2}
+DAYS=${3:-14} #if $3 is empty, default is 14 days.
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 USAGE(){
-    echo -e "USAGE :: sh 19-backup.sg <source> <destination> <days(optional)>"
+    echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
 }
-#check the source dir and destinatio are provided
+#check the source and destination are provided
+
 if [ $# -lt 2 ]
 then
     USAGE
+    exit 1
 fi
 
-if [! -d SOURCE_DIR ]
+if [ ! -d $SOURCE_DIR ]
 then
-    echo "$SOURCE_DIR does not exists please check"
+    echo "$SOURCE_DIR does not exist...Please check"
 fi
 
-if [! -d DESTINATION_DIR ]
+if [ ! -d $DEST_DIR ]
 then
-    echo "$DESTINATION_DIR does not exists please check"
+    echo "$DEST_DIR does not exist...Please check"
 fi
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
 
-echo "Files:$FILES"
+echo "Files: $FILES"
 
 if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
 then
@@ -50,5 +57,4 @@ then
     fi
 else
     echo "No files older than $DAYS"
-fi       
-    
+fi
